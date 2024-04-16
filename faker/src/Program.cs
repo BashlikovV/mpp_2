92360e83b15ced2dd979065cd481d5e8e329dac8
Bashlikov Uladzimir <volodia413314@gmail.com>
2024-04-16 17:01:32 +0600

    test

--- faker/src/Program.cs
+++ faker/src/Program.cs
@@ -7,14 +7,29 @@ namespace faker {
         public static void Main(){ 
             var faker = new FakerImpl();
             var filledObj = faker.create<FillableClass>();
-            Console.WriteLine(filledObj.getStr());
+            Console.WriteLine(filledObj.toString());
         }
     }
 
-    class FillableClass(string str = "")
+    class FillableClass(string str = "", int id = 0, char smth = 'c')
     {
         public string getStr() {
             return str;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public char getSmth()
+        {
+            return smth;
+        }
+
+        public string toString()
+        {
+            return "str: " + str + ", id: " + id + ", smth: " + smth + ";";
         }
     }
 
