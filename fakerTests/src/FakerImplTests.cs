170f08ca3857163ee0d985f6608eb5d68fcd5900
Bashlikov Uladzimir <volodia413314@gmail.com>
2024-04-16 17:06:02 +0600

    tests;

---
+++ fakerTests/src/FakerImplTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using faker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fakerTests;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace faker.Tests
+{
+    [TestClass()]
+    public class FakerImplTests
+    {
+
+        [TestMethod]
+        public void defaultClassTest()
+        {
+            var faker = new FakerImpl();
+            var obj = faker.create<ExampleClass>();
+            obj.field.Should().NotBe(float.NaN);
+            obj.property.Should().NotBe(float.NaN);
+            foreach (var list in obj.list)
+            {
+                list.Should().NotBeNull();
+                foreach (var item in list)
+                {
+                    item.Should().NotBeNull();
+                    item.field.Should().NotBe(float.NaN);
+                    item.property.Should().NotBe(float.NaN);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void recursiveDetectionTest()
+        {
+            var action = () =>
+            {
+                var faker = new FakerImpl();
+                faker.create<RecursiveClass1>();
+            };
+            action.Should().Throw<Exception>();
+        }
+    }
+}
---
+++ fakerTests/src/classes/FakerKonfiig.cs
@@ -0,0 +1,78 @@
+﻿using faker;
+
+namespace fakerTests
+{
+    public class TestClass1
+    {
+        public int f1;
+
+        public TestClass2 f2;
+    }
+
+    public class TestClass2
+    {
+        public int f1;
+        public int f2;
+        public string f3;
+
+        public TestClass1 f4;
+
+        public int proc()
+        {
+            return 9;
+        }
+    }
+
+    public class GenInt : ICustomGenerator<int>
+    {
+        public int generate()
+        {
+            return 9;
+        }
+    }
+
+    public class GenString : ICustomGenerator<string>
+    {
+        public string generate()
+        {
+            return "9";
+        }
+    }
+
+    public class GenTestClass1 : ICustomGenerator<TestClass1>
+    {
+        public TestClass1 generate()
+        {
+            return new TestClass1
+            {
+                f1 = 9,
+                f2 = null
+            };
+        }
+    }
+
+    public class RecursiveClass1
+    {
+        public RecursiveClass2 cycled;
+    }
+
+    public class RecursiveClass2
+    {
+        public RecursiveClass1 test;
+    }
+
+    public class ExampleClass
+    {
+        public float field = float.NaN;
+
+        public float property { get; set; } = float.NaN;
+
+        public List<List<ExampleClassInner>> list;
+    }
+
+    public class ExampleClassInner
+    {
+        public float field = float.NaN;
+        public float property = float.NaN;
+    }
+}
